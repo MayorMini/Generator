0259fe2a3d61556c1b1d70389bab0d0c1a0af9c4
Tony <asolomon484@gmail.com>
2023-04-11 20:07:01 +0100

    commit

---
+++ myScript.js
@@ -0,0 +1,46 @@
+const text=document.getElementById("quote");
+const author=document.getElementById("author");
+const tweetButton=document.getElementById("tweet");
+
+let intervalId;
+
+async function myInterval() {
+    //api for quotes
+    var url="https://type.fit/api/quotes";    
+
+    // fetch the data from api
+    const response= await fetch(url);
+
+    //convert response to json and store it in quotes array
+    const allQuotes = await response.json();
+
+    // Generates a random number between 0 and the length of the quotes array
+    const indx = Math.floor(Math.random()*allQuotes.length);
+
+    //Store the quote present at the randomly generated index
+    const quote=allQuotes[indx].text;
+
+    //Store the author of the respective quote
+    const auth =allQuotes[indx].author;
+
+    if(auth==null)
+    {
+        author.innerHTML = "Anonymous";
+    } else {
+        author.innerHTML = "~ "+auth;
+    }
+
+    //function to dynamically display the quote and the author
+    text.innerHTML=quote;
+
+    //tweet the quote
+    tweetButton.href="https://twitter.com/intent/tweet?text="+quote+" ~ "+auth;
+}
+
+function myStartFunction() {
+    intervalId = setInterval(myInterval, 1000);
+}
+
+function myStopFunction() {
+    clearInterval(intervalId);
+}
